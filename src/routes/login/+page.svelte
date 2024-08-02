1c8192355d463c21c1f148e57cc5c5707a431211
martinleib <m@martinleib.uy>
2024-08-02 02:03:02 -0300

    feat: login and register pages

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<form action="#" method="post" class="container mt-5" on:submit|preventDefault>
+    <div class="mb-3">
+        <label for="emailInput" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" required>
+    </div>
+    <div class="mb-3">
+        <label for="passwordInput" class="form-label">Password</label>
+        <input type="password" class="form-control" id="passwordInput" required>
+    </div>
+    <div class="d-flex justify-content-between align-items-center">
+        <input type="submit" value="Login" class="btn btn-primary">
+        <span class="text-end">
+            <label for="referRegister">Don't have an account?</label>
+            <br />
+            <a href="/register" id="referRegister">Create an account</a>
+        </span>
+    </div>
+</form>
---
+++ src/routes/register/+page.svelte
@@ -0,0 +1,19 @@
+<form action="#" method="post" class="container mt-5" id="registerForm" on:submit|preventDefault>
+    <div class="mb-3">
+        <label for="emailInput" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" required>
+        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+        <label for="passwordInput" class="form-label">Password</label>
+        <input type="password" class="form-control" id="passwordInput" required>
+    </div>
+    <div class="d-flex justify-content-between align-items-center">
+        <input type="submit" value="Register" class="btn btn-primary">
+        <span class="text-end">
+            <label for="referLogin">Already have an account?</label>
+            <br />
+            <a href="/login" id="referLogin">Login</a>
+        </span>
+    </div>
+</form>
